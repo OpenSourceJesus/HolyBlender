d85cda1659e0ea8596444eb97024346d56ac39a3
GileadCosman <gileadcosman@gmail.com>
2024-07-01 07:56:37 -0700

    Added ScreenToWorldPoint support for running C# scripts in BlenderPlugin

--- RunCSInBlender.py
+++ RunCSInBlender.py
@@ -35,12 +35,51 @@ def OnKeyRelease (key):
 keyboardListener = keyboard.Listener(on_press=OnKeyPress, on_release=OnKeyRelease)
 keyboardListener.start()
 
+def Dehomogenize (v):
+	ret = mathutils.Vector((v[0] / v[3], v[1] / v[3], v[2] / v[3]))
+	return ret
+
+def InverseMatrix (m):
+	output = mathutils.Matrix([[0,0,0,0], [0,0,0,0], [0,0,0,0], [0,0,0,0]])
+	determinant = m.determinant()
+	for i in range(4):
+		for j in range(4):
+			temp = mathutils.Matrix([[0,0,0], [0,0,0], [0,0,0]])
+			col = 0
+			for x in range(4):
+				if x != i:
+					row = 0
+					for y in range(4):
+						if y != j:
+							temp[col][row] = m[x][y]
+							row += 1
+					col += 1
+			tempDeterminant = temp.determinant()
+			total = i + j
+			if total % 2:
+				sign = -1
+			else:
+				sign = 1
+			output[j][i] = (sign * tempDeterminant) / determinant
+	return output
+
+def ScreenToWorldPoint (screenPoint):
+	region = bpy.context.region
+	regionCenter = mathutils.Vector((region.x + region.width / 2, region.y + region.height / 2))
+	output = mathutils.Vector((2 * (screenPoint.x - regionCenter.x) / region.width,
+		2 * (screenPoint.y - regionCenter.y) / region.height,
+		2 * screenPoint.z - 1,
+		1))
+	inversePerspectiveMatrix = InverseMatrix(bpy.context.space_data.region_3d.perspective_matrix)
+	output = inversePerspectiveMatrix @ output 
+	output = Dehomogenize(output)
+	return output
+
 def Run (filePath : str, obj):
 	global mouseButtonsPressed_
 	outputCode = 'self = bpy.data.objects[\'' + obj.name + '\']' + '''
 mouseController_ = mouse.Controller()
-mousePosition_ = mathutils.Vector((mouseController_.position[0], mouseController_.position[1]))
-'''
+mousePosition_ = mathutils.Vector((mouseController_.position[0], mouseController_.position[1]))'''
 	outputCode += open(filePath, 'rb').read().decode('utf-8')
 	print(outputCode)
 
